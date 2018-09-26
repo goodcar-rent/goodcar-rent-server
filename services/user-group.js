5867918dcc6787e7e017f48fecb92b1f5ec06024
deksden@air <deksden@gmail.com>
2018-09-26 21:14:20 +0700

    - (enh) user-group: added createData, createSystemData methods - (enh) test for createData, createSystemData methods

--- services/user-group.js
+++ services/user-group.js
@@ -1,6 +1,9 @@
 import _ from 'lodash'
 import uuid from 'uuid/v4'
 
+import dataUserGroupSystem from '../data/user-group-system'
+import dataUserGroup from '../data/user-group'
+
 const _userGroup = []
 
 /*
@@ -24,16 +27,27 @@ const _userGroup = []
 export const systemTypeNone = null
 
 export const systemTypeAdmin = 'Admin'
-
 export const systemTypeGuest = 'Guest'
-
 export const systemTypeLoggedIn = 'LoggedIn'
 
 export default (app) => {
-  return {
+  let _systemGroupAdmin = null
+  let _systemGroupGuest = null
+  let _systemGroupLoggedIn = null
+
+  const Model = {
+    systemGroupAdmin: () => _systemGroupAdmin,
+    systemGroupGuest: () => _systemGroupGuest,
+    systemGroupLoggedIn: () => _systemGroupLoggedIn,
     findById: (id) => Promise.resolve(_.find(_userGroup, { id })),
     findOne: (opt) => Promise.resolve(_.find(_userGroup, [Object.keys(opt.where)[0], Object.values(opt.where)[0]])),
-    findAll: (opt) => Promise.resolve(_.filter(_userGroup, [Object.keys(opt.where)[0], Object.values(opt.where)[0]])),
+    findAll: (opt) => {
+      if (opt) {
+        return Promise.resolve(_.filter(_userGroup, [Object.keys(opt.where)[0], Object.values(opt.where)[0]]))
+      } else {
+        return Promise.resolve(_userGroup)
+      }
+    },
     findGroupsForUser: (user) => Promise.resolve(_.filter(_userGroup, (item) => (_.index(item.users, user) !== -1))),
     count: () => Promise.resolve(_userGroup.length),
     create: (item) => {
@@ -44,22 +58,48 @@ export default (app) => {
       _userGroup.push(item)
       return Promise.resolve(item)
     },
-    addUser: (groupId, user) => {
+    addUser: (groupId, userId) => {
       const group = _.find(_userGroup, { id: groupId })
       if (!group) {
         return Promise.reject(group)
       }
-      _.union(group.users, [user])
+      _.union(group.users, [userId])
       return Promise.resolve(group)
     },
-    removeUser: (id, userId) => {
-      const group = _.find(_userGroup, { id })
+    removeUser: (groupId, userId) => {
+      const group = _.find(_userGroup, { id: groupId })
       if (!group) {
         return Promise.reject(group)
       }
       _.pull(group.users, [userId])
       return Promise.resolve(group)
     },
+    createSystemData: () => {
+      Model.ClearData()
+      _systemGroupAdmin = null
+      _systemGroupGuest = null
+      _systemGroupLoggedIn = null
+      const arr = dataUserGroupSystem.map((item) => {
+        Model.create(item)
+          .then((newItem) => {
+            if (!newItem) throw Error('Fail to create new UserGroup')
+
+            if (newItem.systemType && newItem.systemType === systemTypeAdmin) {
+              if (_systemGroupAdmin) throw new Error('SystemData have duplicate Admin group')
+              _systemGroupAdmin = newItem.id
+            } else if (newItem.systemType && newItem.systemType === systemTypeGuest) {
+              if (_systemGroupGuest) throw new Error('SystemData have duplicate Guest group')
+              _systemGroupGuest = newItem.id
+            } else if (newItem.systemType && newItem.systemType === systemTypeLoggedIn) {
+              if (_systemGroupLoggedIn) throw new Error('SystemData have duplicate LoggedIn group')
+              _systemGroupLoggedIn = newItem.id
+            }
+          })
+      })
+      return Promise.all(arr)
+    },
+    createData: () => Promise.all(dataUserGroup.map((item) => Model.create(item))),
     ClearData: () => Promise.resolve(_userGroup.length = 0)
   }
+  return Model
 }
--- test/services/test-user-group.js
+++ test/services/test-user-group.js
@@ -1,4 +1,4 @@
-import { describe, it, beforeEach } from 'mocha'
+import { describe, it, before, beforeEach } from 'mocha'
 import chai, { expect } from 'chai'
 import dirtyChai from 'dirty-chai'
 import App from '../../app'
@@ -15,7 +15,6 @@ describe('user-group:', () => {
     { id: 1, name: '1' },
     { id: 2, name: '2' }
   ]
-  console.log(systemTypeAdmin)
 
   describe('add/remove method', () => {
     beforeEach(function (done) {
@@ -29,8 +28,6 @@ describe('user-group:', () => {
       UserGroup.create({ name: 'admin', systemType: systemTypeAdmin })
         .then((ug) => {
           userGroup = ug
-          console.log(ug)
-          console.log(systemTypeAdmin)
           expect(userGroup).to.exist()
           expect(userGroup.id).to.exist()
           expect(userGroup.systemType).is.equal(systemTypeAdmin)
@@ -47,6 +44,75 @@ describe('user-group:', () => {
         .then((user2) => {
           expect(user2).to.exist()
           expect(UserGroup.users.length).is.equal(2)
+          done()
+        })
+        .catch((err) => done(err))
+    })
+    it('should remove items', (done) => {
+      UserGroup.create({ name: 'admin', systemType: systemTypeAdmin })
+        .then((ug) => {
+          userGroup = ug
+          expect(userGroup).to.exist()
+          return UserGroup.count()
+        })
+        .then((cnt) => {
+          expect(cnt).is.equal(1)
+          return UserGroup.addUser(userGroup, users[0])
+        })
+        .then((user1) => {
+          expect(user1).to.exist()
+          return UserGroup.addUser(userGroup, users[1])
+        })
+        .then((user2) => {
+          expect(user2).to.exist()
+          expect(UserGroup.users.length).is.equal(2)
+          return UserGroup.removeUser(users[0].id)
+        })
+        .then(() => {
+          expect(UserGroup.users.length).is.equal(1)
+          return UserGroup.removeUser(users[1].id)
+        })
+        .then(() => {
+          expect(UserGroup.users.length).is.equal(0)
+          done()
+        })
+        .catch((err) => done(err))
+    })
+  })
+  describe('create_x_Data methods', () => {
+    before((done) => {
+      app.models.ClearData()
+        .then(() => done())
+    })
+    it('should create system data', (done) => {
+      UserGroup.createSystemData()
+        .then(() => UserGroup.count())
+        .then((cnt) => {
+          expect(cnt).is.equal(3)
+          return UserGroup.findAll()
+        })
+        .then((allGroups) => {
+          expect(allGroups.length).is.equal(3)
+          expect(UserGroup.systemGroupAdmin()).is.not.null()
+          expect(UserGroup.systemGroupGuest()).is.not.null()
+          expect(UserGroup.systemGroupLoggedIn()).is.not.null()
+          done()
+        })
+        .catch((err) => done(err))
+    })
+    it('should create other data', (done) => {
+      UserGroup.createSystemData()
+        .then(() => UserGroup.createData())
+        .then(() => UserGroup.count())
+        .then((cnt) => {
+          expect(cnt).is.equal(5)
+          return UserGroup.findAll()
+        })
+        .then((allGroups) => {
+          expect(allGroups.length).is.equal(5)
+          expect(UserGroup.systemGroupAdmin()).is.not.null()
+          expect(UserGroup.systemGroupGuest()).is.not.null()
+          expect(UserGroup.systemGroupLoggedIn()).is.not.null()
           done()
         })
         .catch((err) => done(err))
