aee8040e0102873082154b01d11e3b179a08c7a5
deksden@air <deksden@gmail.com>
2019-04-14 14:53:03 +0700

    - (enh) WIP model-knex: start implementing knex engine

---
+++ services/model-storage-knex/invite.js
@@ -0,0 +1,76 @@
+import _ from 'lodash'
+import uuid from 'uuid/v4'
+import {
+  genericClearData,
+  genericCount, genericCreate, genericDelete, genericDeleteAll,
+  genericFindAll,
+  genericFindById,
+  genericFindOne,
+  genericInit, genericUpdate
+} from './generic-knex'
+
+/* Invite:
+  * id : uuid
+  * expireAt : date
+  * registeredUser : -> User.id
+  * disabled : boolean
+  * email: invited to this email
+  * assignUserGroups: [] array of user group Ids to assign for user created via this invite
+*/
+const Model = {
+  name: 'Invite',
+  props: [
+    {
+      name: 'id',
+      type: 'id',
+      default: () => uuid()
+    },
+    {
+      name: 'expireAt',
+      type: 'datetime',
+      default: () => Date.now()
+    },
+    {
+      name: 'registeredUser',
+      type: 'ref',
+      default: null
+    },
+    {
+      name: 'createdBy',
+      type: 'ref',
+      default: null
+    },
+    {
+      name: 'disabled',
+      type: 'boolean',
+      default: false
+    },
+    {
+      name: 'email',
+      type: 'text',
+      default: null
+    },
+    {
+      name: 'assignUserGroups',
+      type: 'refs',
+      default: []
+    }
+  ]
+}
+
+export default module.exports = (app) => {
+  Model.app = app
+  const aModel = {
+    initData: genericInit(Model),
+    clearData: genericClearData(Model),
+    findById: genericFindById(Model),
+    findOne: genericFindOne(Model),
+    findAll: genericFindAll(Model),
+    count: genericCount(Model),
+    delete: genericDelete(Model),
+    deleteAll: genericDeleteAll(Model),
+    create: genericCreate(Model),
+    update: genericUpdate(Model)
+  }
+  return aModel
+}
