d50b040a01f77523780f3d65f8228b6a135630fb
deksden@air <deksden@gmail.com>
2019-05-02 12:51:40 +0700

    - (enh) acl: aclObjects save/load, list/set via storage (file as for now)

--- services/acl-storage-memory.js
+++ services/acl-storage-memory.js
@@ -1,8 +1,15 @@
 import _ from 'lodash'
+import { promisify } from 'util'
+import fs from 'fs'
+
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const aclObjectsFile = '/../data/acl-objects.json'
 
 export default (app) => {
   const Storage = {
-    aclStorage: []
+    aclStorage: [],
+    aclObjects: []
   }
 
   return _.merge(Storage, {
@@ -12,6 +19,24 @@ export default (app) => {
     add: (item) => {
       Storage.aclStorage.push(item)
       return Storage.aclStorage.length
+    },
+    aclObjectsList: () => Promise.resolve(Storage.aclObjects),
+    aclObjectsSet: (data) => {
+      Storage.aclObjects = data
+      return Promise.reslove(data)
+    },
+    aclObjectsLoad: () => {
+      return readFile(`${__dirname}${aclObjectsFile}`)
+        .then((fileContents) => JSON.parse(fileContents))
+        .then((data) => {
+          Storage.aclObjects = data
+          return data
+        })
+        .catch((err) => Promise.reject(err))
+    },
+    aclObjectsSave: () => {
+      return writeFile(`${__dirname}${aclObjectsFile}`, JSON.stringify(Storage.aclObjects))
+        .catch((err) => Promise.reject(err))
     }
   })
 }
