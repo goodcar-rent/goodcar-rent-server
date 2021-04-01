7afed0fd78ff870068bc66cbc3588e7eeb85573e
deksden <deksden@deksden.com>
2021-04-01 11:00:17 +0700

    feat (vnext): start building new ver

--- src/app-vnext.js
+++ src/app-vnext.js
@@ -1,6 +1,12 @@
 import ServerBricks from './core/server-bricks'
 import cl from 'json-colorizer'
 
+const opt = {
+  appName: 'goodcar-rent-server',
+  version: '0.3.0',
+  modulePath: '../modules'
+
+}
 ServerBricks()
   .then((app) => {
     console.log(cl(JSON.stringify(app, null, 2), { pretty: true }))
--- src/core/server-bricks.js
+++ src/core/server-bricks.js
@@ -3,7 +3,39 @@
 */
 import Path from 'path'
 
-const modulePath = '../modules'
+
+export const initSync = (app, opt) => {
+  // load modules
+  if (opt.bricks === undefined || opt.bricks === null) {
+    opt.bricks = {}
+    opt.bricks.auth = 'auth-bearer'
+    // opt.bricks.access = 'access'
+  }
+  return app
+}
+
+export const init = async (app, opt) => {
+  if (!opt) {
+    throw Error('server-bricks.init: opt param missing')
+  }
+
+  if (!opt.bricks) {
+    throw Error('server-bricks.init: opt.bricks param missing')
+  }
+
+  const modulePath = opt.modulePath ? opt.modulePath : '../modules'
+
+  Object.keys(opt.bricks).map(async (moduleName) => {
+    const Module = await import(Path.join(modulePath, opt.bricks[moduleName]))
+    const moduleOpt = opt.bricks[moduleName] ? opt.bricks[moduleName].opt : undefined
+
+    app.bricks[moduleName] = Module.default(app, moduleOpt)
+    app.bricks[moduleName].name = moduleName
+    app.bricks[moduleName].opt = moduleOpt
+  })
+
+  return Promise.resolve(app)
+}
 
 export default async (opt) => {
   if (opt === undefined || opt === null) {
@@ -32,21 +64,7 @@ export default async (opt) => {
     bricks: {}
   }
 
-  // load modules
-  if (opt.bricks === undefined || opt.bricks === null) {
-    opt.bricks = {}
-    opt.bricks.auth = 'auth-bearer'
-    // opt.bricks.access = 'access'
-  }
+  initSync(app, opt)
 
-  Object.keys(opt.bricks).map(async (moduleName) => {
-    const Module = await import(Path.join(modulePath, opt.bricks[moduleName]))
-    const moduleOpt = opt.bricks[moduleName] ? opt.bricks[moduleName].opt : undefined
-
-    app.bricks[moduleName] = Module.default(app, moduleOpt)
-    app.bricks[moduleName].name = moduleName
-    app.bricks[moduleName].opt = moduleOpt
-  })
-
-  return Promise.resolve(app)
+  return await init(app, opt)
 }
